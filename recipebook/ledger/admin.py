0f778f1accd9a63184a8ceead03be3a214aa401e
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-03-07 14:47:44 +0800

    Lab 2
    I added Recipe, Ingredient, and RecipeIngredient models. I added an admin page and an admin panel for each model.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    """Creates Ingredient Admin Panel."""
+    model = Ingredient
+
+    list_display = ('ingredient_name',)
+    list_filter = ('ingredient_name',)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    """Creates Recipe Admin Panel."""
+    model = Recipe
+
+    list_display = ('recipe_name',)
+    list_filter = ('recipe_name',)
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    """Creates RecipeIngredient Admin Panel."""
+    model = RecipeIngredient
+
+    list_display = ('quantity',)
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,77 +1,13 @@
 from django.shortcuts import render
-
-recipes_data = [
-    {
-        "id": 1,
-        "name": "Recipe 1",
-        "ingredients":
-        [
-            {
-                "name": "tomato",
-                "quantity": "3pcs"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "pork",
-                "quantity": "1kg"
-            },
-            {
-                "name": "water",
-                "quantity": "1L"
-            },
-            {
-                "name": "sinigang mix",
-                "quantity": "1 packet"
-            },
-        ]
-    },
-    {
-        "id": 2,
-        "name": "Recipe 2",
-        "ingredients":
-        [
-            {
-                "name": "garlic",
-                "quantity": "1 head"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "vinegar",
-                "quantity": "1/2cup"
-            },
-            {
-                "name": "water",
-                "quantity": "1 cup"
-            },
-            {
-                "name": "salt",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "whole black peppers",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "pork",
-                "quantity": "1 kilo"
-            },
-        ]
-    }
-]
-
+from .models import Recipe
 
 def recipe_list(request):
     """Displays recipe list page."""
-    return render(request, 'recipe_list.html', {"recipes": recipes_data})
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe_list.html', {"recipes": recipes})
 
 
 def recipe_ingredients(request, recipe_id):
     """Displays the corresponding ingredients of each recipe."""
-    recipe = next((r for r in recipes_data if r["id"] == recipe_id), None)
+    recipe = Recipe.objects.filter(id=recipe_id).first()
     return render(request, 'recipe_ingredients.html', {"recipe": recipe})

f770a8c0b5f26b8a3f44d587db69b10c8bc2a4c1
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-04-05 15:31:02 +0800

    lab4-fix
    I added a feature wherein you can add the ingredients when creating a recipe. I also fixed some bugs in the redirect after creating a new recipe.

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,15 +1,31 @@
 from django import forms
-from .models import Recipe, RecipeImage
+from .models import Recipe, RecipeImage, Ingredient
 
 class RecipeForm(forms.ModelForm):
     """Creates form to add recipes."""
+    ingredient = forms.ModelChoiceField(
+        queryset = Ingredient.objects.all(),
+        empty_label ="Select an ingredient",
+        widget = forms.Select(attrs={"class": "form-control"}),
+        required = True
+    )
+
+    quantity = forms.CharField(max_length=100, required=True)
+
     class Meta:
         model = Recipe
-        fields = ['recipe_name', 'recipe_author',]
+        fields = ["recipe_name", "recipe_author"]
+
+
+class IngredientForm(forms.ModelForm):
+    """Creates form to add ingredients."""
+    class Meta:
+        model = Ingredient
+        fields = ["ingredient_name"]
 
 
 class RecipeImageForm(forms.ModelForm):
     """Creates form to add recipe images."""
     class Meta:
         model = RecipeImage
-        fields = ['image','description',]
+        fields = ["image","description"]
